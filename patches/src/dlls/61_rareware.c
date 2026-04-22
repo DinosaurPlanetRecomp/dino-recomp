f06a4e01a74ecaa82a26dc3daa53abbc3ba36d9b
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2026-04-21 21:29:08 -0400

    Decomp update

--- patches/src/dlls/61_rareware.c
+++ patches/src/dlls/61_rareware.c
@@ -9,49 +9,66 @@
 
 #include "recomp/dlls/engine/61_rareware_recomp.h"
 
-extern s32 data_0;
-extern s8 data_4;
+#define KEYFRAME_40 40      //Logo fade-in start
+#define KEYFRAME_50 50      //Logo faded in
+#define KEYFRAME_285 285    //Logo glow start
+#define KEYFRAME_620 620    //Screen fade to black
 
-extern s8 bss_0;
-extern s8 bss_1;
-extern s8 bss_2;
-extern f32 bss_4;
-extern f32 bss_8;
+typedef enum {
+    Rareware_STATE_Initial = 0,
+    Rareware_STATE_Fading_In = 1,
+    Rareware_STATE_Visible = 2,
+    Rareware_STATE_Glowing = 3
+} RarewareStates;
 
-RECOMP_PATCH s32 dll_61_update1() {
+extern s32 dFrame;
+extern s8 dState;
+
+extern s8 sFadeOutStarted;
+extern s8 sFadeOutTimer;
+extern s8 sCutToNextScreen;
+extern f32 sLogoTimer;
+extern f32 sGlowTimer;
+
+RECOMP_PATCH s32 rareware_update1() {
     s32 delay;
 
+    //Get gUpdateRate, clamped at maximum of 3
     delay = gUpdateRate;
     if (delay > 3) {
         delay = 3;
     }
 
-    if (bss_1 > 0) {
-        bss_1 -= delay;
+    //Decrement fadeout timer, if it's started
+    if (sFadeOutTimer > 0) {
+        sFadeOutTimer -= delay;
     }
 
     // @recomp: Allow skipping
-    if (bss_2 != 0 || (joy_get_pressed_raw(0) & A_BUTTON) != 0) {
+    if (sCutToNextScreen != 0 || (joy_get_pressed_raw(0) & A_BUTTON) != 0) {
         main_set_bits(BIT_44F, 0);
         menu_set(MENU_GAME_SELECT);
     }
 
-    data_0 += gUpdateRate;
-    if (data_0 > 620) {
-        bss_0 = 1;
+    dFrame += gUpdateRate;
+
+    //Start fading out
+    if (dFrame > KEYFRAME_620) {
+        sFadeOutStarted = TRUE;
     }
 
-    if (bss_0 != 0) {
+    if (sFadeOutStarted) {
         gDLL_28_ScreenFade->vtbl->fade(30, SCREEN_FADE_BLACK);
-        bss_1 = 45;
-        bss_2 = 1;
+        sFadeOutTimer = 45;
+        sCutToNextScreen = TRUE; //@bug: cuts immediately, instead of at end of fade-out
     }
 
-    if (data_4 > 0) {
-        bss_4 -= gUpdateRateF;
+    if (dState >= Rareware_STATE_Fading_In) {
+        sLogoTimer -= gUpdateRateF;
     }
-    if (data_4 > 2) {
-        bss_8 -= gUpdateRateF;
+
+    if (dState >= Rareware_STATE_Glowing) {
+        sGlowTimer -= gUpdateRateF;
     }
 
     return 0;
