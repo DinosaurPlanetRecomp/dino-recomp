484372509b96d3c4b5ce95b421ac36aaba070f99
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2026-04-30 19:13:42 -0400

    Sort dbgui options so commonly used ones are at the top

--- patches/src/custom/builtin_dbgui/builtin_dbgui.c
+++ patches/src/custom/builtin_dbgui/builtin_dbgui.c
@@ -14,10 +14,10 @@ void builtin_dbgui(void) {
     if (dbgui_begin_main_menu_bar()) {
         if (dbgui_begin_menu("Debug")) {
             dbgui_menu_item("Warp", &warpOpen);
-            dbgui_menu_item("DLLs", &dllsOpen);
-            dbgui_menu_item("Audio", &audioOpen);
             dbgui_menu_item("Graphics", &graphicsOpen);
             dbgui_menu_item("Memory", &memoryOpen);
+            dbgui_menu_item("Audio", &audioOpen);
+            dbgui_menu_item("DLLs", &dllsOpen);
             dbgui_end_menu();
         }
         if (dbgui_begin_menu("Cheats")) {
