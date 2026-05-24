c6157c209029acf8136d0f05a20a62e2ae80437f
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2026-05-23 21:14:09 -0400

    Decomp update

--- patches/src/dlls/408_IMIceMountain.c
+++ patches/src/dlls/408_IMIceMountain.c
@@ -43,7 +43,7 @@ RECOMP_PATCH void IMIceMountain_do_race(Object *self, IMIceMountain_Data *objdat
         player = get_player();
         snowbike = ((DLL_210_Player*)player->dll)->vtbl->get_vehicle(player);
         if (snowbike) {
-            racePosition = ((DLL_IVehicle*)snowbike->dll)->vtbl->func17(snowbike);
+            racePosition = ((DLL_IVehicle*)snowbike->dll)->vtbl->get_race_position(snowbike);
         } else {
             racePosition = 0;
         }
